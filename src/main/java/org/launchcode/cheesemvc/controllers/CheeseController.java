93bf6d7ca3b49acbf380b05ae4927e0fa33aa11f
Jtaghiza <Jtaghiza@gmail.com>
2017-05-24 18:58:14 -0500

    initial commit

---
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,42 @@
+package org.launchcode.cheesemvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping(value = "cheese")
+public class CheeseController {
+
+    //data only exists while app is running (not a substitute for a database) so list will go away if you restart because static
+    static ArrayList<String> cheeses = new ArrayList<>();
+
+    // Request Path: /cheese
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheeses");
+        return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+        cheeses.add(cheeseName);
+
+        //redirect to /cheese
+        return "redirect:";
+    }
+
+}
